7187985897e0e84cc5e7d4a308166ac1db1df3d2
Johanna Salazar Ramírez <johannasalazarramirez@gmail.com>
2023-11-27 23:37:53 -0600

    SUBIR A GIT

---
+++ Programada_2_Shiny/Práctica_2_Shiny/app.R
@@ -0,0 +1,14 @@
+# Tarea Programada #2 Shiny
+# Johanna Salazar Ramírez
+
+library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(dplyr)
+library(DT)
+
+
+datos <- read.csv("Datos_libertad/datos_libertad.csv")
+
+View(datos)
+JS
