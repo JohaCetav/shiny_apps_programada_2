51210c71a1c0b570337b09f7838bbc668cd0aa3e
Johanna Salazar Ramírez <johannasalazarramirez@gmail.com>
2023-11-27 23:42:24 -0600

    actualizar el documento con el esqueleto de la app shiny

--- Programada_2_Shiny/Práctica_2_Shiny/app.R
+++ Programada_2_Shiny/Práctica_2_Shiny/app.R
@@ -11,4 +11,31 @@ library(DT)
 datos <- read.csv("Datos_libertad/datos_libertad.csv")
 
 View(datos)
-JS
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Libertades Mundiales", titleWidth = 300),
+  
+  dashboardSidebar(
+    selectInput("pais", "Selecciona un País", choices = unique(datos$pais)),
+    sliderInput("ano", "Selecciona el año:", min = 2008, max = 2016, value = c(2008, 2016)),
+    radioButtons("metrica", "Elige visualización:", choices = c("Puntaje", "Ranking")),
+    downloadButton("descargarPDF", "Descargar en PDF")
+  ),
+  
+  dashboardBody(
+    tabsetPanel(
+      tabPanel("Libertad Humana", 
+               plotOutput("plotHumanaLineas"),
+               plotOutput("plotHumanaBarras")
+      ),
+      tabPanel("Libertad Personal", 
+               plotOutput("plotPersonalLineas"),
+               plotOutput("plotPersonalBarras")
+      ),
+      tabPanel("Libertad Económica", 
+               plotOutput("plotEconomicaLineas"),
+               plotOutput("plotEconomicaBarras")
+      )
+    )
+  )
+)
