5dc76d22db80dcbc3477b9f13f2226d0af818af8
Johanna Salazar Ramírez <johannasalazarramirez@gmail.com>
2023-12-02 17:56:09 -0600

    Terminar de darle color y simetría a la app

--- Programada_2_Shiny/Práctica_2_Shiny/app.R
+++ Programada_2_Shiny/Práctica_2_Shiny/app.R
@@ -1,6 +1,9 @@
 # Tarea Programada #2 Shiny
 # Johanna Salazar Ramírez
 
+# Tarea Programada #2 Shiny
+# Johanna Salazar Ramírez
+
 library(shiny)
 library(shinydashboard)
 library(ggplot2)
@@ -10,34 +13,34 @@ library(DT)
 
 datos <- read.csv("Datos_libertad/datos_libertad.csv")
 
-View(datos)
-
-ui <- dashboardPage(
-  dashboardHeader(title = "Libertades Mundiales", titleWidth = 300),
-  
-  dashboardSidebar(
-    selectInput("pais", "Selecciona un País", choices = unique(datos$pais)),
-    sliderInput("ano", "Selecciona el año:", min = 2008, max = 2016, value = c(2008, 2016)),
-    radioButtons("metrica", "Elige visualización:", choices = c("Puntaje", "Ranking")),
-    downloadButton("descargarPDF", "Descargar en PDF")
-  ),
-  
-  dashboardBody(
-    tabsetPanel(
-      tabPanel("Libertad Humana", 
-               plotOutput("plotHumanaLineas"),
-               plotOutput("plotHumanaBarras")
-      ),
-      tabPanel("Libertad Personal", 
-               plotOutput("plotPersonalLineas"),
-               plotOutput("plotPersonalBarras")
-      ),
-      tabPanel("Libertad Económica", 
-               plotOutput("plotEconomicaLineas"),
-               plotOutput("plotEconomicaBarras")
-      )
-    )
-  )
+ui <- dashboardPage(skin = "green",
+                    dashboardHeader(title = "Libertades Mundiales", titleWidth = 270),
+                    
+                    
+                    dashboardSidebar(width = 270,
+                                     selectInput("pais", "Selecciona un País", choices = unique(datos$pais)),
+                                     sliderInput("ano", "Selecciona el año:", min = 2008, max = 2016, value = c(2008, 2016)),
+                                     radioButtons("metrica", "Elige visualización:", choices = c("Puntaje", "Ranking")),
+                                     downloadButton("descargarPDF", "Descargar en PDF")
+                                     
+                    ),
+                    
+                    dashboardBody(
+                      tabsetPanel(
+                        tabPanel("Libertad Humana", 
+                                 plotOutput("plotHumanaLineas"),
+                                 plotOutput("plotHumanaBarras")
+                        ),
+                        tabPanel("Libertad Personal", 
+                                 plotOutput("plotPersonalLineas"),
+                                 plotOutput("plotPersonalBarras")
+                        ),
+                        tabPanel("Libertad Económica", 
+                                 plotOutput("plotEconomicaLineas"),
+                                 plotOutput("plotEconomicaBarras")
+                        )
+                      )
+                    )
 )
 
 
@@ -49,16 +52,17 @@ server <- function(input, output) {
   })
   
   
+  
+  
   render_lineas <- function(metrica, titulo) {
     ggplot(datos_filtrados(), aes(x = anio, y = get(metrica), group = pais, color = pais)) +
-      geom_line() +
+      geom_line(color = "black") + 
       ggtitle(paste(titulo, "-", input$metrica))
   }
   
-  
   render_barras <- function(metrica, titulo) {
     ggplot(datos_filtrados(), aes(x = anio, y = get(metrica), fill = pais)) +
-      geom_bar(stat = "identity", position = "dodge") +
+      geom_bar(stat = "identity", position = "dodge", color = "black", fill = "green") +  
       ggtitle(paste(titulo, "-", input$metrica))
   }
   
@@ -73,7 +77,7 @@ server <- function(input, output) {
     render_barras(metrica, "Libertad Humana")
   })
   
-
+  
   output$plotPersonalLineas <- renderPlot({
     metrica <- ifelse(input$metrica == "Puntaje", "libertad_personal_puntaje", "libertad_personal_ranking")
     render_lineas(metrica, "Libertad Personal")
@@ -95,14 +99,12 @@ server <- function(input, output) {
     render_barras(metrica, "Libertad Económica")
   })
   
- 
-  output$descargarPDF <- downloadHandler(
+  downloadHandler(
     filename = function() {paste("datos_", input$pais, ".pdf", sep = "")},
     content = function(file) {
       metrica <- ifelse(input$metrica == "Puntaje", paste("libertad", input$metrica, sep = "_"), paste("libertad", input$metrica, sep = "_"))
       pdf(file)
       
-    
       if (input$metrica == "Puntaje") {
         print(render_lineas("libertad_humana_puntaje", "Libertad Humana"))
         print(render_barras("libertad_humana_puntaje", "Libertad Humana"))
@@ -111,7 +113,7 @@ server <- function(input, output) {
         print(render_barras("libertad_humana_ranking", "Libertad Humana"))
       }
       
- 
+      
       if (input$metrica == "Puntaje") {
         print(render_lineas("libertad_personal_puntaje", "Libertad Personal"))
         print(render_barras("libertad_personal_puntaje", "Libertad Personal"))
@@ -120,7 +122,7 @@ server <- function(input, output) {
         print(render_barras("libertad_personal_ranking", "Libertad Personal"))
       }
       
-    
+      
       if (input$metrica == "Puntaje") {
         print(render_lineas("libertad_economica_puntaje", "Libertad Económica"))
         print(render_barras("libertad_economica_puntaje", "Libertad Económica"))
